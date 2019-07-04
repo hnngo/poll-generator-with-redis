188134b4a9a33f3e5950e2f9bc87802c87dfaf1e
Nhan <hnngo@ntu.edu.sg>
2019-07-04 14:50:28 +0800

    Adjust the colors of the progress bar

--- views/src/components/poll/PollCard.js
+++ views/src/components/poll/PollCard.js
@@ -14,17 +14,22 @@ const PollCard = (props) => {
   const isAuth = user.auth;
   const isVoted = isAuth && Object.keys(user.auth.votedPolls).includes(poll.poll_id);
   let isAbleToVote = true;
+  let barColor = "#007B4F";
   let boundOption = "unvoted-bound";
   if (isPrivate && !isAuth) {
+    barColor = "#757575";
     isAbleToVote = false;
     boundOption = "close-bound"
   } else if (isVoted) {
+    barColor = "#007bff";
     isAbleToVote = false;
     boundOption = "voted-bound";
   }
 
   const renderChoices = (i) => {
-    if (poll.multiple_choice) {
+    if (!isAbleToVote) {
+      return <div />;
+    } else if (poll.multiple_choice) {
       return (
         <input
           type="checkbox"
@@ -61,95 +66,56 @@ const PollCard = (props) => {
     const scoresNum = poll.scores.map(s => +s);
     const maxVotes = _.max(scoresNum);
     const barProgress = poll.scores.map((s) => {
-      if (maxVotes === 0) {
-        return 0;
-      }
-
-      return (+s) * 100 / maxVotes;
+      return (maxVotes === 0) ? 0 : (+s) * 100 / maxVotes;
     });
 
-    if (isAbleToVote) {
-      return (
-        <div className="poll-vote-form">
-          <form>
-            {
-              poll.options.map((option, i) => {
-                return (
-                  <div key={i} className="row">
-                    <div className="col-1">
-                      {poll.scores[i]}
-                    </div>
-                    <div className="col-11">
+    return (
+      <div className="poll-vote-form">
+        <form>
+          {
+            poll.options.map((option, i) => {
+              return (
+                <div key={i} className="poll-vote-row">
+                  <div className="poll-vote-header">
+                    <div>
                       {renderChoices(i)}
                       {option}
                     </div>
-                    <div className="col-12 py-2">
-                      <div className="progress">
-                        <div
-                          className="progress-bar progress-bar-striped  progress-bar-animated"
-                          role="progressbar"
-                          style={{
-                            width: `${barProgress[i]}%`,
-                            backgroundColor: "#007B4F"
-                          }}
-                          aria-valuenow="10"
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        />
-                      </div>
+                    <div className="poll-scores">
+                      {poll.scores[i]}
                     </div>
                   </div>
-                );
-              })
-            }
-          </form>
-          <button
-            className="btn-main-orange"
-            onClick={() => props.actVotePoll(poll.poll_id, choices)}
-          >
-            Submit Vote
-          </button>
-        </div>
-      )
-    }
-
-    return (
-      <ul>
-        {
-          poll.options.map((option, i) => {
-            return (
-              <li
-                key={i}
-              >
-                <div className="row">
-                  <div className="col-1">
-                    {poll.scores[i]}
-                  </div>
-                  <div className="col-11">
-                    {option}
-                  </div>
-                  <div className="col-12 py-2">
-                    <div className="progress">
-                      <div
-                        className="progress-bar progress-bar-striped progress-bar-animated"
-                        role="progressbar"
-                        style={{
-                          width: `${barProgress[i]}%`,
-                          backgroundColor: "#007B4F"
-                        }}
-                        aria-valuenow="10"
-                        aria-valuemin="0"
-                        aria-valuemax="100"
-                      />
-                    </div>
+                  <div className="progress poll-progress">
+                    <div
+                      className="progress-bar progress-bar-striped  progress-bar-animated"
+                      role="progressbar"
+                      style={{
+                        width: `${barProgress[i]}%`,
+                        backgroundColor: barColor
+                      }}
+                      aria-valuenow="10"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    />
                   </div>
                 </div>
-              </li>
-            );
-          })
+              );
+            })
+          }
+        </form>
+        {
+          isAbleToVote ?
+            <button
+              className="btn-main-orange"
+              onClick={() => props.actVotePoll(poll.poll_id, choices)}
+            >
+              Submit Vote
+            </button>
+            :
+            <div />
         }
-      </ul>
-    )
+      </div>
+    );
   };
 
   return (
